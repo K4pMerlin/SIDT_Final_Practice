b0ed360e6ba17032ad5758400d59cd0b0f216d99
zhuqian1029 <1766026711@qq.com>
2024-09-19 20:03:05 +0800

    编写自动执行的初始化函数，它在包被加载时运行。 它首先获取当前的课程时间信息，并更新全局变量 curWeekNum、curWeekday 和 curLessonNum。 CurCourseTime函数在read_data中，计算当前时间下的周数、星期几和课程节数 findBuildingMapWithKeys函数用于处理教学楼的映射，1-3区与4-5区的处理略有不同，分开编写处理代码

---
+++ Backend/process/run.go
@@ -0,0 +1,49 @@
+package process
+
+var ChoseSpecialDate = new(CourseDate)
+
+type CourseDate struct {
+	Chose bool `json:"chose"`
+	Month *int `json:"month,omitempty"`
+	Day   *int `json:"day,omitempty"`
+	Hour  *int `json:"hour,omitempty"`
+	Min   *int `json:"min,omitempty"`
+}
+
+type RespTeachInfo struct {
+	Room         string `json:"room"`
+	Faculty      string `json:"faculty"`
+	CourseName   string `json:"courseName"`
+	TeacherName  string `json:"teacherName"`
+	TeacherTitle string `json:"teacherTitle"`
+	CourseTime   string `json:"courseTime"`
+	CourseType   string `json:"courseType"`
+}
+
+// BuildingTeachInfos 每个学部各个教学楼的课程信息
+type BuildingTeachInfos struct {
+	Building string          `json:"building"`
+	Infos    []RespTeachInfo `json:"infos"`
+}
+
+var buildingMapArray []map[string][]TeachInfo
+
+// 5个学部
+var respTeachInfos = make([][]BuildingTeachInfos, 5)
+
+var curWeekNum = atomic.Int32{}
+var curWeekday = atomic.Int32{}
+var curLessonNum = atomic.Int32{}
+
+
+func init() {
+	num, weekday, lessonNum := CurCourseTime()
+	curWeekNum.Store(int32(num))
+	curWeekday.Store(int32(weekday))
+	curLessonNum.Store(int32(lessonNum))
+
+	for i := 1; i <= 5; i++ {
+		buildingMap, _ := findBuildingMapWithKeys(i)
+		buildingMapArray = append(buildingMapArray, buildingMap)
+	}
+}
