eafa05f48705dc02f10b6df260c940aa946dafa8
liudanyi <2774566461@qq.com>
2024-09-21 08:19:34 +0800

    html搭建

--- 方案二：辅助听课工具/front/front/src/App.vue
+++ 方案二：辅助听课工具/front/front/src/App.vue
@@ -1,10 +1,26 @@
-
-
 <template>
-  <div id="app">
-    <router-view />
+  <!--  <div class="bg-gradient-to-tl from-cyan-200/50 to-blue-400/60 h-[100vh]">-->
+  <div class="bg-[#ffffff] text-[#000001]">
+    <div class="bg-[#dda15e]/50 h-[100%]">
+      <div
+        class="flex w-[100vw] h-[11vw] bg-[#dda15e] border-[0.5vw] border-[#bc6c25] place-items-center place-content-center mt">
+        <h1 class="text-[5vw]">辅助听课工具</h1>
+      </div>
+
+      <div
+        class="flex h-[11vw] bg-[#dda15e]/50 place-items-center place-content-center rounded-3xl mt-[3.5vw] ml-[6vw] mr-[6vw]">
+        <div class="text-[4.5vw]"></div>
+      </div>
+
+
+      <!--<Menu></Menu>-->
+    </div>
+    <!--    <div class="bg-[#dda15e]/50 h-[70vh]"></div>-->
   </div>
+
+
 </template>
+
 
 <style>
 html,
